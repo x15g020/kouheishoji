b238dee57b959ecb55408ef1683024b3c7822044
unknown <x15g020@PC505N007.chiba-fjb.ac.jp>
2015-09-11 11:44:01 +0900

    no message

--- src/Test09110911.java
+++ src/Test09110911.java
@@ -3,7 +3,8 @@ public class Test09110911 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("あぐｄがｋｇｓｋｊ");
+		System.out.println(""
+				+ "ぐｄがｋｇｓｋｊ");
 
 	}
 
